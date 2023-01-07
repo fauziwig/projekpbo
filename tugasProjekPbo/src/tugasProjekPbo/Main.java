e15ac55b624ae6d24e1bd36cea4adf0986e77754
Fauzi Triagung Wiguna <101690263+fauziwig@users.noreply.github.com>
2023-01-07 22:16:54 +0700

    Update Main.java

--- tugasProjekPbo/src/tugasProjekPbo/Main.java
+++ tugasProjekPbo/src/tugasProjekPbo/Main.java
@@ -1,68 +1,88 @@
 package tugasProjekPbo;
 
-import java.util.Scanner;
+import javax.swing.JOptionPane;
 
-public class Main {
-
-	public static void main(String[] args) {
-		
-		String isi,pil_string,ulang_string;
+public class Main{
+    public static void main (String [] args){
+      String isi,pil_string,ulang_string;
         int pil,ulang;
-        DataIndukNasabah din;
-        Deposit depo= new Deposit();
-        din = depo;
-        Scanner input = new Scanner(System.in);
-        
+        DataIndukNasabah xx;
+               
 
-   do {
-            System.out.println("1. Pendaftaran Nasabah Deposit ");
-            System.out.println("2. Cetak Data ");
-            System.out.println("3. Tambah Saldo ");
-            System.out.println("4. Cetak Saldo ");
-            System.out.println("5. Daftar Tabungan ");
-            System.out.println("6. Keluar ");
-            
-            System.out.println(" Masukkan pilihan anda : ");
-            pil_string = input.nextLine();
-        
+   do { //perulangan
+            isi="1. PENDATAAN DEPOSIT\n"+"2. PENDATAAN TABUNGAN REGULER\n"+"3. KELUAR";
+            pil_string= JOptionPane.showInputDialog(null,isi, "BANK KRUD",JOptionPane.QUESTION_MESSAGE);
             pil=Integer.parseInt(pil_string);
-            switch(pil) { 
-            //perkondisian
+            switch(pil) { //perkondisian
                 case 1:{
-                  din.inputDataNasabah();
-                  din.inputJenis();
-                  
-                  System.out.println(" Input Selesai, klik Ok untuk lanjut ");
+                        int pil2;
+                        Deposit satu = new Deposit();
+                        xx = satu;
+                        do { //perulangan
+                            isi="1. Pendaftaran Data Deposit Baru\n"+"2. Cetak Data Nasabah\n"+"3. Tambah Saldo\n"
+                                + "4. Cetak Saldo\n"+ "5. Penarikan\n"+"6. Keluar\n"+"Masukkan pilihan anda :";
+                            pil_string=JOptionPane.showInputDialog(null,isi, "BANK KRUD", JOptionPane.QUESTION_MESSAGE);
+                            pil2=Integer.parseInt(pil_string);
+                        switch(pil2) { //perkondisian
+                        case 1:{
+                            xx.indftNasabah();
+                            xx.inputJenis();
+                            JOptionPane.showMessageDialog(null, "Input Selesai, klik OK untuk melanjutkan","INPUT SELESAI",JOptionPane.INFORMATION_MESSAGE);
+                            break;}
+                        case 2:{xx.cetak_nasabah();
+                            break;}
+                        case 3: {xx.penyetoran();
+                            break;}
+                        case 4:{xx.cetak_saldo();
+                            break;}
+                        case 5:{xx.penarikan(); 
+                        		xx.cetak_penarikan();
+                            break;}
+                        case 6:JOptionPane.showMessageDialog(null,"Klik OK untuk melanjutkan");
                         break;
+                        }
+                        ulang_string=JOptionPane.showInputDialog("Mau transaksi lagi?\nJika YA input 1\n" +
+                        "MENUJU MENU UTAMA input 0\n"+"masukkan pilihan :");
+                        ulang=Integer.parseInt(ulang_string);
+                    }while(ulang==1);
                 }
-                case 2:{din.cetak_nasabah();
-                        break;}
-                case 3: {din.penyetoran();
-                break;}
-                case 4:{din.cetak_saldo();
-                break;}
-                case 5: {
+                case 2:{
+                int pil2;
                         Tabungan tab1=new Tabungan();
-                        din = tab1;
-                        din.inputDataNasabah();
-                        din.inputJenis();
-                        din.cetak_nasabah();
-                break;}
-                case 6:{
-                	System.out.println(" Klik Ok untuk keluar ");
-                break;}
-                
-  
-                
+                        xx = tab1;
+                        do { //perulangan
+                            isi="1. Pendaftaran Data Tabungan Baru\n"+"2. Cetak Data Nasabah\n"+"3. Tambah Saldo\n"
+                                + "4. Cetak Saldo\n"+"5. Keluar\n"+"Masukkan pilihan anda :";
+                            pil_string=JOptionPane.showInputDialog(null,isi, "BANK KRUD", JOptionPane.QUESTION_MESSAGE);
+                            pil2=Integer.parseInt(pil_string);
+                        switch(pil2) { //perkondisian
+                        case 1:{
+                            xx.indftNasabah();
+                            xx.inputJenis();
+                            JOptionPane.showMessageDialog(null, "Input Selesai, klik OK untuk melanjutkan","INPUT SELESAI",JOptionPane.INFORMATION_MESSAGE);
+                            break;}
+                        case 2:{xx.cetak_nasabah();
+                            break;}
+                        case 3: {xx.penyetoran();
+                            break;}
+                        case 4:{xx.cetak_saldo();
+                            break;}
+                        case 5:JOptionPane.showMessageDialog(null,"Klik OK untuk melanjutkan");
+                        break;
+                        }
+                        ulang_string=JOptionPane.showInputDialog("Mau transaksi lagi?\nJika YA input 1\n" +
+                        "MENUJU MENU UTAMA input 0\n"+"masukkan pilihan :");
+                        ulang=Integer.parseInt(ulang_string);
+                    }while(ulang==1);
+                }
+                case 3:JOptionPane.showMessageDialog(null,"Klik OK untuk melanjutkan");
+                break;
             }
-            
-            System.out.println("Mau transaksi lagi? Jika Ya = 1, Tidak = 0, masukkan pilihan anda : ");
-            ulang_string = input.nextLine();
-          
+            ulang_string=JOptionPane.showInputDialog("Mau transaksi lagi?\nMENU UTAMA input 1\n" +
+                    "KELUAR input 0\n"+"masukkan pilihan :");
             ulang=Integer.parseInt(ulang_string);
         }
-        while(ulang==1);   
-
-	}
-
+        while(ulang==1);      
+    }
+    
 }
